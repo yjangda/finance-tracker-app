c5137c522e2a53a5396b5207239d490581092ece
Yusuf Jangda <yusufjangda@Yusufs-MBP.lan>
2025-01-07 13:48:51 -0800

    basic home page functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const loggedIn = {firstName: 'Yusuf'}
+    return (
+        <section className='home'>
+            <div className = "home-content">
+                <header>
+                    <HeaderBox
+                        type = 'greeting'
+                        title = 'Welcome to Horizon'
+                        user = {loggedIn?.firstName || 'Guest'} 
+                        subtext = 'Access and manage your account with ease'
+                    />
+
+                    <TotalBalanceBox
+                        accounts = {[]}
+                        totalBanks = {1}
+                        totalCurrentBalance={1250.32}
+
+                    />
+
+
+
+                </header>
+
+            </div>
+        </section>
+    )
 }
 
 export default Home
